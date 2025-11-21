2f789d62b06e1340a6de413a5f543db2d9986f1a
lizasha228-max <lizasha228@gmail.com>
2025-11-22 00:03:35 +0200

    create new page

--- src/TextComponent.jsx
+++
@@ -1,9 +0,0 @@
-export default function Texted ({numberCount,letter}){
- return (
-    <div className="textContent">
-        <p>{numberCount}</p>
-        <span>{letter}</span>
-    </div>
- )
-}
-
