c95f2e00df09576a80ef2b132114df98b4aeb076
lizasha228-max <lizasha228@gmail.com>
2025-11-25 14:52:25 +0200

    create new components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Profile = () => {
+    return    <div className="content">
+      <div className="contentImg"><img src="https://kartin.papik.pro/uploads/posts/2023-07/thumbs/1689402671_kartin-papik-pro-p-kartinki-plyazh-dlinnaya-8.jpg"/></div>
+      <div>ava + scription</div>
+      <div>my posts</div>
+      <div>new posts</div>
+      <div>Post 1</div>
+      <div>Post 2</div>
+    </div>
+} 
+
+export default Profile
