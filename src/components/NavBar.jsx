824856104f7c03db5569d9a159d0823783fa3e5d
lizasha228-max <lizasha228@gmail.com>
2025-11-28 23:10:29 +0200

    add new modules

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import classes from "./NavBar.module.css";
 
 const NavBar = () => {
-    return    <nav className="nav">
-        <div>
+    return    <nav className={classes.nav}>
+        <div className={classes.item}>
           <a>Profile</a>
         </div>
-        <div>
+        <div className={`${classes.item} ${classes.active}`}>
           <a>Messeges</a>
         </div>
-          <div>
+         <div className={classes.item}>
           <a>News</a>
         </div>
-          <div>
+          <div className={classes.item}>
           <a>Music</a>
         </div>
-         <div>
+        <div className={classes.item}>
           <a>Setings</a>
         </div>
       </nav>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import classes from "./Profile.module.css";
 
 const Profile = () => {
-    return    <div className="content">
-      <div className="contentImg"><img src="https://kartin.papik.pro/uploads/posts/2023-07/thumbs/1689402671_kartin-papik-pro-p-kartinki-plyazh-dlinnaya-8.jpg"/></div>
+    return    <div className={classes.content}>
+      <div className={classes.contentImg}><img src="https://kartin.papik.pro/uploads/posts/2023-07/thumbs/1689402671_kartin-papik-pro-p-kartinki-plyazh-dlinnaya-8.jpg"/></div>
       <div>ava + scription</div>
       <div>my posts</div>
       <div>new posts</div>
-      <div>Post 1</div>
-      <div>Post 2</div>
+      <div className="post">
+      <div className="item">Post 1</div>
+      <div className="item">Post 2</div>
+    </div>
     </div>
 } 
 
