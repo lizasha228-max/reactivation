1aa099452d387769cd001689e6b16277e14972be
lizasha228-max <lizasha228@gmail.com>
2025-11-30 22:17:24 +0200

    added new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
-import Profile from './components/Profile/Profile';
+// import Profile from './components/Profile/Profile';
+
 
 const App = () => {
   return (
     <div className="app-wrapper">
        <Header />
        <NavBar />
-       <Profile />
+       <div class="app-wrapper-content">
+        <Dialogs />
+       </div>
+       {/* <Profile /> */}
     </div>
   )
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import MyPosts from "./MyPosts/MyPosts";
 import classes from "./Profile.module.css";
 
 const Profile = () => {
-    return    <div className={classes.content}>
+    return    <div>
       <div className={classes.contentImg}><img src="https://kartin.papik.pro/uploads/posts/2023-07/thumbs/1689402671_kartin-papik-pro-p-kartinki-plyazh-dlinnaya-8.jpg"/></div>
       <div>ava + scription</div>
       <MyPosts />
